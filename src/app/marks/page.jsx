714ad95fa45b52d96d34746eb0d2a4212dd92cff
nimeshjanaka <nimeshjanaka.1995@gmail.com>
2024-06-25 15:39:21 +0530

    Update:Homepage

--- src/app/marks/page.jsx
+++ src/app/marks/page.jsx
@@ -1,7 +1,12 @@
+import Marks from "@/components/marks/page";
 import React from "react";
 
 const page = () => {
-  return <div>marks</div>;
+  return (
+    <div>
+      <Marks />
+    </div>
+  );
 };
 
 export default page;

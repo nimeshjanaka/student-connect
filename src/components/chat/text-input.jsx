b26b463bbbb687b11b909e8f3824f1cb8ef81f34
Thiyunu <dinal.bandara@gmail.com>
2024-07-04 15:56:14 +0530

    update: Added Function to add the input to the chat window

--- src/components/chat/text-input.jsx
+++ src/components/chat/text-input.jsx
@@ -4,23 +4,37 @@ import { Input } from "../../components/ui/input";
 import { Button } from "../../components/ui/button";
 import { FaArrowUp } from "react-icons/fa";
 
+const TextInput = ({ onInputSubmit }) => {
+  const [inputValue, setInputValue] = useState("");
+  const defaultPlaceholder = 'Enter your text here';
+  const [placeholder,setPlaceholder] = useState(defaultPlaceholder)
 
+  const handleInputSubmit = () => {
+    onInputSubmit(inputValue);
+    setInputValue('');
+    setPlaceholder(defaultPlaceholder)
 
-const TextInput = () => {
-  
+  };
+
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   return (
-    <div className=" flex items-center text-black bg-slate-300 px-4 py-2 max-w-screen h-15 ml-5 mt-40">
-
+    <div className=" flex items-center text-black  rounded-md bg-slate-300 px-4 py-2 max-w-screen h-15 ml-5 ">
       <Input
         className="bg-transparent text-black w-full focus:outline-none text-xs border-none focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
-
-        placeholder="Type Here"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder={placeholder}
       />
 
-      <Button className=" rounded-3xl bg-transparent">
+      <button
+        className=" rounded-3xl bg-transparent"
+        onClick={handleInputSubmit}
+      >
         <FaArrowUp className="w-4 h-4 " color="black" />
-      </Button>
+      </button>
     </div>
   );
 };
